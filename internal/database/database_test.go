c05b9f28679997642bb6d86e7e8d8f29b8a6f9a2
tvairakt <t.vairaktaris@gmail.com>
2026-07-06 22:09:33 +0300

    test(database): add failing test for Connect

--- internal/database/database_test.go
+++ internal/database/database_test.go
@@ -1,1 +1,19 @@
 package database
+
+import "testing"
+
+func TestConnect(t *testing.T) {
+	db, err := Connect(":memory:")
+	if err != nil {
+		t.Fatalf("Connect returned an error: %v", err)
+	}
+	defer db.Close()
+
+	var foreignKeysEnabled int
+	if err := db.QueryRow("PRAGMA foreign_keys").Scan(&foreignKeysEnabled); err != nil {
+		t.Fatalf("failed to query foreign_keys pragma: %v", err)
+	}
+	if foreignKeysEnabled != 1 {
+		t.Errorf("expected foreign_keys = 1, got %d", foreignKeysEnabled)
+	}
+}
